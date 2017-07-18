81fd81656b774679b9efb7caa968308ca8943775
Anton Dosov <dosantinc@gmail.com>
2017-07-18 07:24:13 +0300

    Add Redux Devtools extension

--- react-demo-app/src/store.js
+++ react-demo-app/src/store.js
@@ -2,7 +2,12 @@ import { createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const enhancer = composeWithDevTools(
+  applyMiddleware(thunk, logger)  
+);
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
